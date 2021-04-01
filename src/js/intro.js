8f0705785196cfaf8171b716349f55d3262a2102
Artem Safin <artemsafin.webdev@gmail.com>
2021-04-02 00:26:07 +0300

    Мобилка кругов

--- src/js/intro.js
+++ src/js/intro.js
@@ -1,9 +1,9 @@
 export default function intro() {
+    if (window.matchMedia('(max-width: 640px)').matches) return;
     const elements = Array.from(document.querySelectorAll('.js-intro'));
 
     elements.forEach(element => {
         const sectors = Array.from(element.querySelectorAll('.intro__cards-sector'));
-
 
         sectors.forEach(sector => {
             sector.addEventListener('mouseenter', () => {
@@ -11,19 +11,19 @@ export default function intro() {
                 sector.classList.add('active');
                 sectors.forEach(otherSector => {
                     if (otherSector === sector) {
-                        otherSector.classList.remove('faded')
+                        otherSector.classList.remove('faded');
                     } else {
-                        otherSector.classList.add('faded')
+                        otherSector.classList.add('faded');
                     }
                 });
-            })
+            });
             sector.addEventListener('mouseleave', () => {
                 sector.classList.remove('active');
                 element.classList.remove('hovered');
                 sectors.forEach(sector => {
-                    sector.classList.remove('faded')
-                })
-            })
-        })
-    })
+                    sector.classList.remove('faded');
+                });
+            });
+        });
+    });
 }
