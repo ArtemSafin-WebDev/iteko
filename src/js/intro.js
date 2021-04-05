1f6b042d981faedcb205473dd4d28cb7ba155936
Artem Safin <artemsafin.webdev@gmail.com>
2021-04-05 14:20:10 +0300

    Вернул круги

--- src/js/intro.js
+++ src/js/intro.js
@@ -1,14 +1,14 @@
 export default function intro() {
+  
     const elements = Array.from(document.querySelectorAll('.js-intro'));
+
 
     if (!window.matchMedia('(max-width: 640px)').matches) {
         elements.forEach(element => {
-            const cards = Array.from(element.querySelectorAll('.intro__card'));
             const sectors = Array.from(element.querySelectorAll('.intro__cards-sector'));
-
-            cards.forEach(card => {
-                card.addEventListener('mouseenter', () => {
-                    const sector = card.closest('.intro__cards-sector');
+    
+            sectors.forEach(sector => {
+                sector.addEventListener('mouseenter', () => {
                     element.classList.add('hovered');
                     sector.classList.add('active');
                     sectors.forEach(otherSector => {
@@ -19,8 +19,7 @@ export default function intro() {
                         }
                     });
                 });
-                card.addEventListener('mouseleave', () => {
-                    const sector = card.closest('.intro__cards-sector');
+                sector.addEventListener('mouseleave', () => {
                     sector.classList.remove('active');
                     element.classList.remove('hovered');
                     sectors.forEach(sector => {
@@ -34,6 +33,7 @@ export default function intro() {
             const scrollWrapper = element.querySelector('.intro__cards-wrapper');
             const scrollProgress = element.querySelector('.intro__scroll-progress');
 
+
             console.log('scroll wrapper offset width', scrollWrapper.offsetWidth, scrollWrapper.scrollWidth);
 
             if (scrollWrapper.scrollWidth <= scrollWrapper.offsetWidth) {
@@ -41,12 +41,14 @@ export default function intro() {
                 return;
             }
 
+
             scrollWrapper.addEventListener('scroll', () => {
                 const progress = scrollWrapper.scrollLeft / (scrollWrapper.scrollWidth - scrollWrapper.offsetWidth);
 
                 scrollProgress.style.setProperty('--scroll-progress', progress.toFixed(2));
-                console.log('Progress', progress);
-            });
-        });
+                console.log('Progress', progress)
+            })
+        })
     }
+   
 }
