6ce90fdeb889cad6d30fa14978ffa72fbd33c3cd
Artem Safin <artemsafin.webdev@gmail.com>
2021-04-05 07:06:05 +0300

    Адаптив

--- src/js/solutionExamples.js
+++ src/js/solutionExamples.js
@@ -1,6 +1,6 @@
-import { Swiper, Navigation, EffectFade } from 'swiper';
+import { Swiper, Navigation, Pagination } from 'swiper';
 
-Swiper.use([Navigation, EffectFade]);
+Swiper.use([Navigation, Pagination]);
 
 export default function solutionExamples() {
     const elements = Array.from(document.querySelectorAll('.js-solution-examples-slider'));
@@ -10,12 +10,23 @@ export default function solutionExamples() {
         new Swiper(container, {
             watchOverflow: true,
             touchStartPreventDefault: false,
-            slidesPerView: 3,
             speed: 700,
-            spaceBetween: 40,
+            slidesPerView: 1,
+            spaceBetween: 10,
             navigation: {
                 nextEl: element.querySelector('.solution-examples__arrow--next'),
                 prevEl: element.querySelector('.solution-examples__arrow--prev')
+            },
+            pagination: {
+                el: element.querySelector('.solution-examples__slider-pagination'),
+                type: 'bullets',
+                clickable: true
+            },
+            breakpoints: {
+                641: {
+                    slidesPerView: 3,
+                    spaceBetween: 40
+                }
             }
         });
     });
