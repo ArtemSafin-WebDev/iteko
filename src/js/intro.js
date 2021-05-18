beb406d4b12f2e87b7d87c7eafc12be09188e28e
Artem Safin <artemsafin.webdev@gmail.com>
2021-05-18 19:07:42 +0300

    Начал добавлять вид списком

--- src/js/intro.js
+++ src/js/intro.js
@@ -1,18 +1,22 @@
-export default function intro() {
-  
-    const elements = Array.from(document.querySelectorAll('.js-intro'));
+import { gsap } from 'gsap';
+import { Flip } from 'gsap/Flip';
 
+gsap.registerPlugin(Flip);
+
+export default function intro() {
+    const elements = Array.from(document.querySelectorAll('.js-intro'));
 
     if (!window.matchMedia('(max-width: 640px)').matches) {
         elements.forEach(element => {
             const sectors = Array.from(element.querySelectorAll('.intro__cards-sector'));
-    
+            const chaosView = element.querySelector('.js-chaos-view');
+            const listView = element.querySelector('.js-ordered-view');
+
             sectors.forEach((sector, sectorIndex) => {
                 sector.addEventListener('mouseenter', () => {
                     element.classList.add('hovered');
                     sector.classList.add('active');
                     sectors.forEach((otherSector, otherSectorIndex) => {
-                      
                         if (otherSector === sector) {
                             otherSector.classList.remove('faded');
                             element.classList.add(`sector-${otherSectorIndex + 1}-hovered`);
@@ -31,12 +35,48 @@ export default function intro() {
                     });
                 });
             });
+
+            const enterChaosView = () => {
+                chaosView.classList.add('active');
+                listView.classList.remove('active');
+                element.classList.remove('ordered-view');
+            };
+
+            const enterOrderView = () => {
+                chaosView.classList.remove('active');
+                listView.classList.add('active');
+                element.classList.add('ordered-view');
+            };
+
+            window.returnToGrid = () => {
+                const state = Flip.getState(document.querySelectorAll('.intro__card'));
+                element.classList.add('returned-to-grid');
+                Flip.from(state, {
+                    duration: 0.8,
+                    paused: false,
+                    // ease: 'none'
+                });
+            };
+
+            if (chaosView) {
+                chaosView.addEventListener('click', event => {
+                    event.preventDefault();
+                    enterChaosView();
+                });
+            }
+            if (listView) {
+                listView.addEventListener('click', event => {
+                    event.preventDefault();
+                    enterOrderView();
+                });
+            }
+
+            enterChaosView();
         });
     } else {
         elements.forEach(element => {
             const scrollWrapper = element.querySelector('.intro__cards-wrapper');
             const scrollProgress = element.querySelector('.intro__scroll-progress');
-
 
             console.log('scroll wrapper offset width', scrollWrapper.offsetWidth, scrollWrapper.scrollWidth);
 
@@ -45,14 +85,12 @@ export default function intro() {
                 return;
             }
 
-
             scrollWrapper.addEventListener('scroll', () => {
                 const progress = scrollWrapper.scrollLeft / (scrollWrapper.scrollWidth - scrollWrapper.offsetWidth);
 
                 scrollProgress.style.setProperty('--scroll-progress', progress.toFixed(2));
-                console.log('Progress', progress)
-            })
-        })
+                console.log('Progress', progress);
+            });
+        });
     }
-   
 }
