e2937948e2e9ed3fd39631e25813ae59d14e1572
elizavetaShved <eli3avetta.shvedchikova@gmail.com>
2021-07-26 18:08:44 +0300

    save btns

--- src/js/solutionHistoryIteko.js
+++ src/js/solutionHistoryIteko.js
@@ -1,3 +1,37 @@
 export default function solutionHistoryIteko() {
-  console.log(1234)
+  const elements = Array.from(document.querySelectorAll('.solution-history'));
+  let yearsList;
+  let annotationsList;
+
+  let currentYearIdx;
+
+  elements.forEach(element => {
+    yearsList = Array.from(element.querySelectorAll('.solution-history__year-item'));
+    annotationsList = Array.from(element.querySelectorAll('.solution-history__annotation-item'));
+
+    const setCurrentYear = (index) => {
+      if (currentYearIdx || currentYearIdx === 0) {
+        yearsList[currentYearIdx].classList.remove('solution-history__year-item--active');
+        annotationsList[currentYearIdx].classList.remove('solution-history__annotation-item--active');
+      }
+
+      currentYearIdx = index;
+      yearsList[index].classList.add('solution-history__year-item--active');
+      annotationsList[index].classList.add('solution-history__annotation-item--active');
+    }
+
+    setCurrentYear(0);
+
+    yearsList.forEach((yearItem, index) => {
+      yearItem.onclick = () => {
+        setCurrentYear(index);
+      }
+    })
+
+    annotationsList.forEach((annotationItem, index) => {
+      annotationItem.onclick = () => {
+        setCurrentYear(index);
+      }
+    })
+  });
 }
