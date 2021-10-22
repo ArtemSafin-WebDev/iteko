8d229a13b849f265164c674008401cec23ae7db1
Artem Safin <artemsafin.webdev@gmail.com>
2021-10-22 13:50:08 +0300

    По датапикеру

--- src/js/datepicker.js
+++ src/js/datepicker.js
@@ -1,21 +1,27 @@
 import 'bootstrap-datepicker';
 import 'bootstrap-datepicker/js/locales/bootstrap-datepicker.ru';
 
-export default function datepicker() {
+function initializeDatepicker() {
     const elements = Array.from(document.querySelectorAll('.js-datepicker'));
 
     elements.forEach(element => {
         $(element)
             .datepicker({
                 format: 'dd.mm.yyyy',
-                container: '#picker-container',
+                container: element.getAttribute('data-container'),
                 language: 'ru',
                 autoclose: true
             })
             .on('show', function(e) {
-                element.classList.add('datepicker-shown')
-            }).on('hide', function(e) {
-                element.classList.remove('datepicker-shown')
+                element.classList.add('datepicker-shown');
+            })
+            .on('hide', function(e) {
+                element.classList.remove('datepicker-shown');
             });
     });
+}
+
+export default function datepicker() {
+    window.initializeDatepicker = initializeDatepicker;
+    initializeDatepicker();
 }
