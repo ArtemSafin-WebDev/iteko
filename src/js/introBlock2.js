2f10f5d54b7698c04d5ccead5b05690d0e8ef2a9
elizavetaShved <eli3avetta.shvedchikova@gmail.com>
2021-10-27 17:24:35 +0300

    fix

--- src/js/introBlock2.js
+++ src/js/introBlock2.js
@@ -1,12 +1,13 @@
 export default function introBlock2() {
-  const btnClose = document.querySelector('.js-close-block-2');
-  const introElems = document.querySelector('.js-intro');
-  const introBlock2Elems = document.querySelector('.js-intro-block-2');
+    const btnClose = document.querySelector('.js-close-block-2');
+    const introElems = document.querySelector('.js-intro');
+    const introBlock2Elems = document.querySelector('.js-intro-block-2');
 
-  btnClose.onclick = () => {
-    setTimeout(() => {
-      introElems.classList.add('mod-show');
-    }, 300)
-    introBlock2Elems.classList.add('mod-hide');
-  }
+    btnClose.onclick = () => {
+        setTimeout(() => {
+            introElems.classList.add('mod-show');
+            introBlock2Elems.classList.add('mod-no-show');
+        }, 300);
+        introBlock2Elems.classList.add('mod-hide');
+    };
 }
