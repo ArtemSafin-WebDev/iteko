c5ad7dd612439cf32e3b11b7b7a097cabbce7695
Artem Safin <artemsafin.webdev@gmail.com>
2021-11-09 01:30:37 +0300

    Фикс слайдера партнеров

--- src/js/partnersSlider.js
+++ src/js/partnersSlider.js
@@ -8,38 +8,49 @@ export default function partnersSlider() {
     elements.forEach(element => {
         const container = element.querySelector('.swiper-container');
 
-        new Swiper(container, {
+        let options = {
             slidesPerView: 2,
             slidesPerColumn: 4,
             spaceBetween: 20,
-            slidesPerGroup: 1,
+            slidesPerGroup: 2,
             slidesPerColumnFill: 'row',
             watchOverflow: true,
+            speed: 700,
             threshold: 5,
             navigation: {
                 nextEl: element.querySelector('.partners__slider-arrow--prev'),
                 prevEl: element.querySelector('.partners__slider-arrow--next')
-            },
-            breakpoints: {
-                768: {
-                    slidesPerView: 5,
-                    slidesPerColumn: 5,
-                    spaceBetween: 50,
-                    slidesPerGroup: 2,
-                },
-                968: {
-                    slidesPerView: 6,
-                    slidesPerColumn: 5,
-                    spaceBetween: 70,
-                    slidesPerGroup: 2,
-                },
-                1025: {
-                    slidesPerView: 8,
-                    slidesPerColumn: 5,
-                    spaceBetween: 70,
-                    slidesPerGroup: 2,
-                }
             }
-        });
+        };
+
+        if (window.matchMedia('(min-width: 768px)').matches) {
+            options = {
+                ...options,
+                slidesPerView: 5,
+                slidesPerColumn: 5,
+                spaceBetween: 50,
+                slidesPerGroup: 5
+            };
+        }
+        if (window.matchMedia('(min-width: 968px)').matches) {
+            options = {
+                ...options,
+                slidesPerView: 6,
+                slidesPerColumn: 5,
+                spaceBetween: 70,
+                slidesPerGroup: 6
+            };
+        }
+        if (window.matchMedia('(min-width: 1025px)').matches) {
+            options = {
+                ...options,
+                slidesPerView: 8,
+                slidesPerColumn: 5,
+                spaceBetween: 70,
+                slidesPerGroup: 8
+            };
+        }
+
+        new Swiper(container, options);
     });
 }
