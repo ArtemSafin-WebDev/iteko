3db976891c465ee7733941d0d5193ae08b798dc9
gilyazov <gilyazov.aydar@gmail.com>
2021-11-09 09:53:30 +0300

    Формы ОС

--- build/assets/js/backend.js
+++ build/assets/js/backend.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
     var demoForm = document.querySelector('#demo-form');
-
     if (demoForm) {
         demoForm.addEventListener('submit', function(event) {
             event.preventDefault();
@@ -9,18 +8,31 @@ document.addEventListener('DOMContentLoaded', () => {
                     .parsley()
                     .isValid()
             ) {
-                demoForm.reset();
-                $(demoForm)
-                    .parsley()
-                    .reset();
-                if (typeof window.openModal === 'function') {
-                    window.openModal('#demo-success');
-                }
+                let options = {
+                    success: function(data){
+                        if (data.ID > 0) {
+                            demoForm.reset();
+                            $(demoForm)
+                                .parsley()
+                                .reset();
+                            if (typeof window.openModal === 'function') {
+                                window.openModal('#demo-success');
+                            }
+                        } else {
+                            if (typeof window.openModal === 'function') {
+                                window.openModal('#demo-failure');
+                            }
+                        }
+                    },
+                    dataType:  'json',
+                };
+
+                $(demoForm).ajaxSubmit(options);
             }
         });
     }
-    var registrationForm = document.querySelector('#registration-form');
 
+    var registrationForm = document.querySelector('#registration-form');
     if (registrationForm) {
         registrationForm.addEventListener('submit', function(event) {
             event.preventDefault();
@@ -39,8 +51,8 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
     }
-    var vacancyForm = document.querySelector('#vacancy-form');
 
+    var vacancyForm = document.querySelector('#vacancy-form');
     if (vacancyForm) {
         vacancyForm.addEventListener('submit', function(event) {
             event.preventDefault();
@@ -49,15 +61,28 @@ document.addEventListener('DOMContentLoaded', () => {
                     .parsley()
                     .isValid()
             ) {
-                vacancyForm.reset();
-                $(vacancyForm)
-                    .parsley()
-                    .reset();
-                vacancyForm.classList.add('success');
+                let options = {
+                    success: function(data){
+                        if (data.ID > 0) {
+                            vacancyForm.reset();
+                            $(vacancyForm)
+                                .parsley()
+                                .reset();
+                            vacancyForm.classList.add('success');
 
-                setTimeout(function() {
-                    vacancyForm.classList.remove('success');
-                }, 3000);
+                            setTimeout(function() {
+                                vacancyForm.classList.remove('success');
+                            }, 3000);
+                        } else {
+                            if (typeof window.openModal === 'function') {
+                                window.openModal('#demo-failure');
+                            }
+                        }
+                    },
+                    dataType:  'json',
+                };
+
+                $(vacancyForm).ajaxSubmit(options);
             }
         });
     }
