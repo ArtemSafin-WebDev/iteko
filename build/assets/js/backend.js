002aed4fef66982a84f978b1d83323824fde8f80
Artem Safin <artemsafin.webdev@gmail.com>
2021-11-10 17:36:09 +0300

    По задачам

--- build/assets/js/backend.js
+++ build/assets/js/backend.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     var demoForm = document.querySelector('#demo-form');
+
     if (demoForm) {
         demoForm.addEventListener('submit', function(event) {
             event.preventDefault();
@@ -8,31 +9,18 @@ document.addEventListener('DOMContentLoaded', () => {
                     .parsley()
                     .isValid()
             ) {
-                let options = {
-                    success: function(data){
-                        if (data.ID > 0) {
-                            demoForm.reset();
-                            $(demoForm)
-                                .parsley()
-                                .reset();
-                            if (typeof window.openModal === 'function') {
-                                window.openModal('#demo-success');
-                            }
-                        } else {
-                            if (typeof window.openModal === 'function') {
-                                window.openModal('#demo-failure');
-                            }
-                        }
-                    },
-                    dataType:  'json',
-                };
-
-                $(demoForm).ajaxSubmit(options);
+                demoForm.reset();
+                $(demoForm)
+                    .parsley()
+                    .reset();
+                if (typeof window.openModal === 'function') {
+                    window.openModal('#demo-success');
+                }
             }
         });
     }
-
     var registrationForm = document.querySelector('#registration-form');
+
     if (registrationForm) {
         registrationForm.addEventListener('submit', function(event) {
             event.preventDefault();
@@ -41,31 +29,18 @@ document.addEventListener('DOMContentLoaded', () => {
                     .parsley()
                     .isValid()
             ) {
-                let options = {
-                    success: function(data){
-                        if (data.ID > 0) {
-                            registrationForm.reset();
-                            $(registrationForm)
-                                .parsley()
-                                .reset();
-                            if (typeof window.openModal === 'function') {
-                                window.openModal('#registration-success');
-                            }
-                        } else {
-                            if (typeof window.openModal === 'function') {
-                                window.openModal('#demo-failure');
-                            }
-                        }
-                    },
-                    dataType:  'json',
-                };
-
-                $(registrationForm).ajaxSubmit(options);
+                registrationForm.reset();
+                $(registrationForm)
+                    .parsley()
+                    .reset();
+                if (typeof window.openModal === 'function') {
+                    window.openModal('#registration-success');
+                }
             }
         });
     }
-
     var vacancyForm = document.querySelector('#vacancy-form');
+
     if (vacancyForm) {
         vacancyForm.addEventListener('submit', function(event) {
             event.preventDefault();
@@ -74,28 +49,15 @@ document.addEventListener('DOMContentLoaded', () => {
                     .parsley()
                     .isValid()
             ) {
-                let options = {
-                    success: function(data){
-                        if (data.ID > 0) {
-                            vacancyForm.reset();
-                            $(vacancyForm)
-                                .parsley()
-                                .reset();
-                            vacancyForm.classList.add('success');
+                vacancyForm.reset();
+                $(vacancyForm)
+                    .parsley()
+                    .reset();
+                vacancyForm.classList.add('success');
 
-                            setTimeout(function() {
-                                vacancyForm.classList.remove('success');
-                            }, 3000);
-                        } else {
-                            if (typeof window.openModal === 'function') {
-                                window.openModal('#demo-failure');
-                            }
-                        }
-                    },
-                    dataType:  'json',
-                };
-
-                $(vacancyForm).ajaxSubmit(options);
+                setTimeout(function() {
+                    vacancyForm.classList.remove('success');
+                }, 3000);
             }
         });
     }
--- src/js/careerCatalogFilter.js
+++ src/js/careerCatalogFilter.js
@@ -1,25 +1,30 @@
 export default function careerCatalogFilter() {
-    const elements = Array.from(document.querySelectorAll('.js-career-catalog-filter'));
+    function initializeCareerCatalogFilter() {
+        const elements = Array.from(document.querySelectorAll('.js-career-catalog-filter'));
 
-    elements.forEach(element => {
-        const btn = element.querySelector('.career-catalog__filter-btn');
-        
+        elements.forEach(element => {
+            const btn = element.querySelector('.career-catalog__filter-btn');
 
-        btn.addEventListener('click', event => {
-            event.preventDefault();
-            element.classList.toggle('active');
-        })
+            btn.addEventListener('click', event => {
+                event.preventDefault();
+                element.classList.toggle('active');
+            });
 
-        element.addEventListener('mouseenter', () => {
-            element.classList.add('active');
-        })
-        element.addEventListener('mouseleave', () => {
-            element.classList.remove('active');
-        })
+            element.addEventListener('mouseenter', () => {
+                element.classList.add('active');
+            });
+            element.addEventListener('mouseleave', () => {
+                element.classList.remove('active');
+            });
 
-        document.addEventListener('click', event => {
-            if (event.target.matches('.js-career-catalog-filter') || event.target.closest('.js-career-catalog-filter')) return;
-            element.classList.remove('active');
-        })
-    })
+            document.addEventListener('click', event => {
+                if (event.target.matches('.js-career-catalog-filter') || event.target.closest('.js-career-catalog-filter')) return;
+                element.classList.remove('active');
+            });
+        });
+    }
+
+    initializeCareerCatalogFilter();
+    
+    window.initializeCareerCatalogFilter = initializeCareerCatalogFilter;
 }
