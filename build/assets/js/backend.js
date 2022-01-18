5a55330cf93161721ae63afa27bfb4ac0eb0d47e
gilyazov <gilyazov.aydar@gmail.com>
2022-01-18 12:51:57 +0300

    Рекапча v3

--- build/assets/js/backend.js
+++ build/assets/js/backend.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const siteKey = "6LfMQx4eAAAAAOOHMGr_O548h-f4JDDwc8V_Gvwo";
     var demoForm = document.querySelector('#demo-form');
-
     if (demoForm) {
         demoForm.addEventListener('submit', function(event) {
             event.preventDefault();
@@ -9,18 +9,38 @@ document.addEventListener('DOMContentLoaded', () => {
                     .parsley()
                     .isValid()
             ) {
-                demoForm.reset();
-                $(demoForm)
-                    .parsley()
-                    .reset();
-                if (typeof window.openModal === 'function') {
-                    window.openModal('#demo-success');
-                }
+                grecaptcha.ready(function() {
+                    grecaptcha.execute(siteKey, {action: 'homepage'}).then(function(token) {
+                        let recaptchaResponse = demoForm.querySelector('[name = g-recaptcha-response]');
+                        recaptchaResponse.value = token;
+
+                        let options = {
+                            success: function(data){
+                                if (data.ID > 0) {
+                                    demoForm.reset();
+                                    $(demoForm)
+                                        .parsley()
+                                        .reset();
+                                    if (typeof window.openModal === 'function') {
+                                        window.openModal('#demo-success');
+                                    }
+                                } else {
+                                    if (typeof window.openModal === 'function') {
+                                        window.openModal('#demo-failure');
+                                    }
+                                }
+                            },
+                            dataType:  'json',
+                        };
+
+                        $(demoForm).ajaxSubmit(options);
+                    });
+                });
             }
         });
     }
-    var registrationForm = document.querySelector('#registration-form');
 
+    var registrationForm = document.querySelector('#registration-form');
     if (registrationForm) {
         registrationForm.addEventListener('submit', function(event) {
             event.preventDefault();
@@ -29,18 +49,39 @@ document.addEventListener('DOMContentLoaded', () => {
                     .parsley()
                     .isValid()
             ) {
-                registrationForm.reset();
-                $(registrationForm)
-                    .parsley()
-                    .reset();
-                if (typeof window.openModal === 'function') {
-                    window.openModal('#registration-success');
-                }
+
+                grecaptcha.ready(function() {
+                    grecaptcha.execute(siteKey, {action: 'homepage'}).then(function(token) {
+                        let recaptchaResponse = demoForm.querySelector('[name = g-recaptcha-response]');
+                        recaptchaResponse.value = token;
+
+                        let options = {
+                            success: function(data){
+                                if (data.ID > 0) {
+                                    registrationForm.reset();
+                                    $(registrationForm)
+                                        .parsley()
+                                        .reset();
+                                    if (typeof window.openModal === 'function') {
+                                        window.openModal('#registration-success');
+                                    }
+                                } else {
+                                    if (typeof window.openModal === 'function') {
+                                        window.openModal('#demo-failure');
+                                    }
+                                }
+                            },
+                            dataType:  'json',
+                        };
+
+                        $(registrationForm).ajaxSubmit(options);
+                    });
+                });
             }
         });
     }
-    var vacancyForm = document.querySelector('#vacancy-form');
 
+    var vacancyForm = document.querySelector('#vacancy-form');
     if (vacancyForm) {
         vacancyForm.addEventListener('submit', function(event) {
             event.preventDefault();
@@ -49,15 +90,38 @@ document.addEventListener('DOMContentLoaded', () => {
                     .parsley()
                     .isValid()
             ) {
-                vacancyForm.reset();
-                $(vacancyForm)
-                    .parsley()
-                    .reset();
-                vacancyForm.classList.add('success');
+                grecaptcha.ready(function() {
+                    grecaptcha.execute(siteKey, {action: 'homepage'}).then(function(token) {
+                        let recaptchaResponse = demoForm.querySelector('[name = g-recaptcha-response]');
+                        recaptchaResponse.value = token;
 
-                setTimeout(function() {
-                    vacancyForm.classList.remove('success');
-                }, 3000);
+                        let options = {
+                            success: function(data){
+                                if (data.ID > 0) {
+                                    vacancyForm.reset();
+                                    $(vacancyForm)
+                                        .parsley()
+                                        .reset();
+                                    vacancyForm.classList.add('success');
+
+                                    setTimeout(function() {
+                                        vacancyForm.classList.remove('success');
+                                    }, 3000);
+                                } else {
+                                    if (typeof window.openModal === 'function') {
+                                        window.openModal('#demo-failure');
+                                    }
+                                }
+                            },
+                            dataType:  'json',
+                        };
+
+                        setTimeout(function() {
+                            vacancyForm.classList.remove('success');
+                        }, 3000);
+                        $(vacancyForm).ajaxSubmit(options);
+                    });
+                });
             }
         });
     }
